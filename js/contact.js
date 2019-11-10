edf0edf8a3f6b544a1b47ffb45441b45e712ef3d
publsoft <publsoftware@gmail.com>
2019-11-10 14:46:20 +0300

    develop

--- js/contact.js
+++ js/contact.js
@@ -20,12 +20,30 @@ $(document).ready(function() {
     // validate contactForm form
     $(function() {
       $("#contactForm").submit(function(event) {
+        event.preventDefault();
+
+        var fullName = $("#fullname").val();
+        var email = $("#email").val();
+        var phone_number = $("#phone_number").val();
+        var message = $("#message").val();
+
+        if (
+          fullName === "" ||
+          email === "" ||
+          phone_number === "" ||
+          message === ""
+        ) {
+          alert("Lütfen tüm alanları doldurunuz!");
+          return;
+        }
+
         db.collection("contact")
           .add({
-            fullname: $("#fullname").val(),
-            email: $("#email").val(),
-            phone_number: $("#phone_number").val(),
-            message: $("#message").val()
+            fullname: fullName,
+            email: email,
+            phone_number: phone_number,
+            message: message,
+            created_date: new Date()
           })
           .then(function(docRef) {
             alert(
@@ -39,86 +57,7 @@ $(document).ready(function() {
           .catch(function(error) {
             alert("Bilinmeyen bir hata oluştu: ", error);
           });
-
-        event.preventDefault();
       });
-
-      /*
-      $("#contactForm").validate({
-        rules: {
-          name: {
-            required: true,
-            minlength: 2
-          },
-          subject: {
-            required: true,
-            minlength: 4
-          },
-          number: {
-            required: true,
-            minlength: 5
-          },
-          email: {
-            required: true,
-            email: true
-          },
-          message: {
-            required: true,
-            minlength: 20
-          }
-        },
-        messages: {
-          name: {
-            required: "come on, you have a name, don't you?",
-            minlength: "your name must consist of at least 2 characters"
-          },
-          subject: {
-            required: "come on, you have a subject, don't you?",
-            minlength: "your subject must consist of at least 4 characters"
-          },
-          number: {
-            required: "come on, you have a number, don't you?",
-            minlength: "your Number must consist of at least 5 characters"
-          },
-          email: {
-            required: "no email, no message"
-          },
-          message: {
-            required:
-              "um...yea, you have to write something to send this form.",
-            minlength: "thats all? really?"
-          }
-        },
-        submitHandler: function(form) {
-          alert(form);
-          /*$(form).ajaxSubmit({
-            type: "POST",
-            data: $(form).serialize(),
-            url: "contact_process.php",
-            success: function() {
-              $("#contactForm :input").attr("disabled", "disabled");
-              $("#contactForm").fadeTo("slow", 1, function() {
-                $(this)
-                  .find(":input")
-                  .attr("disabled", "disabled");
-                $(this)
-                  .find("label")
-                  .css("cursor", "default");
-                $("#success").fadeIn();
-                $(".modal").modal("hide");
-                $("#success").modal("show");
-              });
-            },
-            error: function() {
-              $("#contactForm").fadeTo("slow", 1, function() {
-                $("#error").fadeIn();
-                $(".modal").modal("hide");
-                $("#error").modal("show");
-              });
-            }
-          });
-        }
-      });*/
     });
   })(jQuery);
 });
